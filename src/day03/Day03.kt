8225d6be614f6b646181538796d6f1ae709ab1fa
Henrik Edsparr <henrik.edsparr@gmail.com>
2023-12-04 14:26:11 +0100

    Work in progress

---
+++ src/day03/Day03.kt
@@ -0,0 +1,77 @@
+package day03
+
+import utils.Resources.resourceAsListOfString
+
+typealias Point = Pair<Int, Int>
+
+fun main(){
+    val neighbors = sequenceOf(-1 to 0, 0 to -1, 0 to 1, 1 to 0, -1 to 1, -1 to -1, 1 to 1, 1 to -1)
+    val parts = mutableSetOf<Part>()
+    val symbols = mutableSetOf<Symbol>()
+    val input = resourceAsListOfString("src/day03/Day03.txt")
+
+    fun createPoints(first: Int, start: Int, end: Int): Set<Point>{
+        val newSet = mutableSetOf<Point>()
+        for (i in start..<end){
+            newSet.add(Point(first, i))
+        }
+        return newSet
+    }
+
+
+
+    fun parseInput(input: List<String>) {
+        val regex: Regex = """(?<=[&*@$#%/\-+=])|(?=[&*@$#%/\-+=])""".toRegex()
+        input.forEachIndexed { indexRow, row ->
+            val partsOrSymbols = row.split(".").filterNot { it.isEmpty() }
+                .map { it.split(regex) }.flatten().filterNot { it.isEmpty() }
+            partsOrSymbols.forEachIndexed() { index, it ->
+                when {
+                    it.toIntOrNull() == null -> {
+                        val indices = Regex("""\$it""").findAll(row).map { it.range.first }.toList()
+                        indices.forEach { indice -> symbols.add(Symbol(it, Point(indexRow, indice)))}
+                    }
+                    else -> parts.add(Part(indexRow, it.toInt(), createPoints(indexRow, row.indexOf(it), row.indexOf(it)+it.length)))
+                }
+            }
+        }
+    }
+
+    fun part1(): Int{
+        val partNumbers = mutableSetOf<Pair<Int, Int>>()
+        symbols.forEach { symbol ->
+            val adjacentPoints = neighbors.map { it + symbol.point }.toSet()
+            partNumbers.addAll(parts.map { part -> Part.returnNumberIfIntersecting(part, adjacentPoints) })
+
+        }
+        println(partNumbers.filterNot { it.second == 0 })
+        return partNumbers.sumOf { it.second }
+    }
+
+    parseInput(input)
+    println(parts)
+    println(symbols)
+    println(part1())
+}
+
+private operator fun Point.plus(that: Point): Point =
+    Point(this.first +that.first, this.second+that.second)
+
+data class Symbol(
+    val value: String,
+    val point: Point
+)
+data class Part(
+    val id: Int,
+    val number: Int,
+    val points: Set<Point>
+){
+    companion object{
+        fun returnNumberIfIntersecting(part: Part, points: Set<Point>): Pair<Int, Int> {
+            return when{
+                (part.points intersect points).isNotEmpty()-> Pair(part.id,part.number)
+                else -> Pair(0,0)
+            }
+        }
+    }
+}
